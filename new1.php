6d77d72958309db794905956a228aeeca091f17a
Pranav Bhat <50375979+pranavbhat3105@users.noreply.github.com>
2022-01-10 11:13:17 +0530

    Add files via upload

---
+++ new1.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>GFG- Store Data</title>
+</head>
+
+<body>
+	<center>
+		<h1>Storing Form data in Database</h1>
+
+		<form action="insert.php" method="post">
+			
+			
+<p>
+				<label for="firstName">First Name:</label>
+				<input type="text" name="first_name" id="firstName">
+			</p>
+
+
+
+			
+			
+<p>
+				<label for="lastName">Last Name:</label>
+				<input type="text" name="last_name" id="lastName">
+			</p>
+
+
+
+			
+			
+<p>
+				<label for="Date">Date:</label>
+				<input type="date" name="date" id="Date">
+			</p>
+
+
+			
+			
+			
+<p>
+				<label for="Address">Address:</label>
+				<input type="text" name="address" id="Address">
+			</p>
+
+
+			
+			
+			
+<p>
+				<label for="emailAddress">Email Address:</label>
+				<input type="text" name="email" id="emailAddress">
+			</p>
+
+
+			
+			<input type="submit" value="Submit">
+		</form>
+	</center>
+</body>
+
+</html>
