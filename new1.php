d1e8dd7fda5b75fb33b4737c169120a1c971f759
Pranav Bhat <50375979+pranavbhat3105@users.noreply.github.com>
2022-01-22 11:03:48 +0530

    Update new1.php

--- new1.php
+++ new1.php
@@ -14,7 +14,7 @@
 			
 <p>
 				<label for="firstName">First Name:</label>
-				<input type="text" name="first_name" id="firstName">
+				<input type="text" name="first_name" id="first_name">
 			</p>
 
 
@@ -23,16 +23,14 @@
 			
 <p>
 				<label for="lastName">Last Name:</label>
-				<input type="text" name="last_name" id="lastName">
+				<input type="text" name="last_name" id="last_name">
 			</p>
 
 
-
-			
 			
 <p>
 				<label for="Date">Date:</label>
-				<input type="date" name="date" id="Date">
+				<input type="date" name="date" id="date">
 			</p>
 
 
@@ -41,16 +39,26 @@
 			
 <p>
 				<label for="Address">Address:</label>
-				<input type="text" name="address" id="Address">
+				<input type="text" name="address" id="address">
 			</p>
 
 
-			
+			<label for="flavour">Cake Flavour:</label>
+			<select name="flavour" id="flavour" class="form-control">
+                                            <option value="Black Forest">Black Forest</option>
+                                            <option value="Yellow Butter Cake">Yellow Butter Cake</option>
+                                            <option value="Pound Cake">Pound Cake</option>
+                                            <option value="Red Velvet Cake">Red Velvet Cake</option>
+                                            <option value="Carrot Cake">Carrot Cake</option>
+                                            <option value="Sponge Cake">Sponge Cake</option>
+                                            <option value="Genoise Cake">Genoise Cake</option>
+                                            <option value="Chiffon Cake">Chiffon Cake</option>
+                                        </select>
 			
 			
 <p>
 				<label for="emailAddress">Email Address:</label>
-				<input type="text" name="email" id="emailAddress">
+				<input type="text" name="email" id="email">
 			</p>
 
 
