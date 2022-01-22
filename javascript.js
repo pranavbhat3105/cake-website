63d5c7ebbda7fb9cc466a0c53301a7361cb652c9
Pranav Bhat <50375979+pranavbhat3105@users.noreply.github.com>
2022-01-22 11:04:57 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,58 @@
+var slidePosition = 1;
+SlideShow(slidePosition);
+
+// forward/Back controls
+function plusSlides(n) {
+  SlideShow(slidePosition += n);
+}
+
+//  images controls
+function currentSlide(n) {
+  SlideShow(slidePosition = n);
+}
+
+function SlideShow(n) {
+  var i;
+  var slides = document.getElementsByClassName("Containers");
+  var circles = document.getElementsByClassName("dots");
+  if (n > slides.length) {slidePosition = 1}
+  if (n < 1) {slidePosition = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < circles.length; i++) {
+      circles[i].className = circles[i].className.replace(" enable", "");
+  }
+  slides[slidePosition-1].style.display = "block";
+  circles[slidePosition-1].className += " enable";
+} 
+
+
+
+
+
+
+ // Initialize and add the map
+ function initMap() {
+  // The location of Uluru
+  //15.585447276196795, 73.8118130546306
+  const uluru = { lat:  15.585447276196795, lng: 73.8118130546306 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 18,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
+
+function submit(){
+ 
+  // document.getElementById("RegForm").reset();
+
+
+}
